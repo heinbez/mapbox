8312eebbd7b4312be3e6afbaa968c70db2a57570
Heinrich Bezuidenhout <heinrich@beztdesigns.com>
2015-12-09 05:33:58 +1100

    Update Mapbox.php

--- src/Mapbox.php
+++ src/Mapbox.php
@@ -1,145 +1,145 @@
 <?php
 
-	namespace Mapbox;
+namespace Mapbox;
 
-	class Mapbox {
+class Mapbox {
 
-		/**
-		 * Configuration 
-		 * 
-		 * @var array
-		 */
-		private static $config = array();
+	/**
+	 * Configuration 
+	 * 
+	 * @var array
+	 */
+	private static $config = array();
 
-		/**
-		 * Styles
-		 * 
-		 * @var MapboxStyle 
-		 */
-		private $style;
+	/**
+	 * Styles
+	 * 
+	 * @var MapboxStyle 
+	 */
+	private $style;
 
-		/**
-		 * Coordinates
-		 * 
-		 * @var MapboxPathCollection
-		 */
-		private $coordinates = array();
+	/**
+	 * Coordinates
+	 * 
+	 * @var MapboxPathCollection
+	 */
+	private $coordinates = array();
 
-		/**
-		 * Markers Collection
-		 * 
-		 * @var array
-		 */
-		private $markers = array();
+	/**
+	 * Markers Collection
+	 * 
+	 * @var array
+	 */
+	private $markers = array();
 
-		/**
-		 * Width, Height of Map
-		 * 
-		 * @var integer $w
-		 * @var integer $h
-		 */
-		private $width;
-		private $height;
+	/**
+	 * Width, Height of Map
+	 * 
+	 * @var integer $w
+	 * @var integer $h
+	 */
+	private $width;
+	private $height;
 
-		/**
-		 * Constructor Call
-		 */
-		public function __construct() {}
+	/**
+	 * Constructor Call
+	 */
+	public function __construct() {}
 
-		/**
-		 * Set Configuration options
-		 * 
-		 * @param MapboxConfiguration $config 
-		 */
-		public static function setConfig(MapboxConfiguration $config) {
-			static::$config = $config;
-		}
-
-		/**
-		 * Push Coordinates
-		 * 
-		 * @param  array $cords 
-		 */
-		public function pushCoordinates($cords) {
-			$this->coordinates = $cords;
-		}
-
-		/**
-		 * Push Style
-		 * 
-		 * @param  MapboxStyle $style 
-		 */
-		public function setStyle(MapboxStyle $style) {
-			$this->style = $style->get();
-		}
-
-		/**
-		 * Set Width of Map
-		 * 
-		 * @param Integer $w
-		 */
-		public function setWidth($w) {
-			$this->width = $w;
-		}
-
-		/**
-		 * Set Height of Map
-		 * 
-		 * @param Integer $h
-		 */
-		public function setHeight($h) {
-			$this->height = $h;
-		}
-
-		/**
-		 * Add Marker 
-		 * 
-		 * @param array $options 
-		 */
-		public function addMarker($options) {}
-
-		/**
-		 * Prepare Geo Data
-		 * 
-		 * @return array
-		 */
-		private function geoData() {
-			$_data = [
-				'type' => "Feature"
-			];
-
-			// Push Coordinates Path
-			if($this->coordinates) {
-				$_data['geometry']['type'] = "LineString";
-				$_data['geometry']['coordinates'] = $this->coordinates->collection();
-			}
-			
-			// Push Style Options
-			if($this->style) {
-				$_data['properties'] = $this->style;
-			}
-
-			return urlencode(json_encode($_data));
-		}
-
-		/**
-		 * Generate a Static Mapbox Image 
-		 * 
-		 * @param  MapboxLatLng $options 
-		 * 
-		 * @return string $path
-		 */
-		public function generate() {
-			return sprintf(
-				'%s/%s/geojson(%s)/%s/%sx%s.png?access_token=%s',
-				static::$config->host(),
-				static::$config->style(),
-				$this->geoData(),
-				'auto',
-				$this->width,
-				$this->height,
-				static::$config->key()
-			);
-		}
-
+	/**
+	 * Set Configuration options
+	 * 
+	 * @param MapboxConfiguration $config 
+	 */
+	public static function setConfig(MapboxConfiguration $config) {
+		static::$config = $config;
 	}
+
+	/**
+	 * Push Coordinates
+	 * 
+	 * @param  array $cords 
+	 */
+	public function pushCoordinates($cords) {
+		$this->coordinates = $cords;
+	}
+
+	/**
+	 * Push Style
+	 * 
+	 * @param  MapboxStyle $style 
+	 */
+	public function setStyle(MapboxStyle $style) {
+		$this->style = $style->get();
+	}
+
+	/**
+	 * Set Width of Map
+	 * 
+	 * @param Integer $w
+	 */
+	public function setWidth($w) {
+		$this->width = $w;
+	}
+
+	/**
+	 * Set Height of Map
+	 * 
+	 * @param Integer $h
+	 */
+	public function setHeight($h) {
+		$this->height = $h;
+	}
+
+	/**
+	 * Add Marker 
+	 * 
+	 * @param array $options 
+	 */
+	public function addMarker($options) {}
+
+	/**
+	 * Prepare Geo Data
+	 * 
+	 * @return array
+	 */
+	private function geoData() {
+		$_data = [
+			'type' => "Feature"
+		];
+
+		// Push Coordinates Path
+		if($this->coordinates) {
+			$_data['geometry']['type'] = "LineString";
+			$_data['geometry']['coordinates'] = $this->coordinates->collection();
+		}
+		
+		// Push Style Options
+		if($this->style) {
+			$_data['properties'] = $this->style;
+		}
+
+		return urlencode(json_encode($_data));
+	}
+
+	/**
+	 * Generate a Static Mapbox Image 
+	 * 
+	 * @param  MapboxLatLng $options 
+	 * 
+	 * @return string $path
+	 */
+	public function generate() {
+		return sprintf(
+			'%s/%s/geojson(%s)/%s/%sx%s.png?access_token=%s',
+			static::$config->host(),
+			static::$config->style(),
+			$this->geoData(),
+			'auto',
+			$this->width,
+			$this->height,
+			static::$config->key()
+		);
+	}
+
+}
