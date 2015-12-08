e141709effb6d2b0c00ff315994909d11ac40c4b
Heinrich Bezuidenhout <heinrich@beztdesigns.com>
2015-12-09 05:33:04 +1100

    Create MapboxCollection.php

---
+++ src/MapboxCollection.php
@@ -0,0 +1,44 @@
+<?php 
+
+	namespace Mapbox;
+
+	class MapboxCollection {
+
+		/**
+		 * Collection
+		 * 
+		 * @var array
+		 */
+		private $collection = array();
+
+		/**
+		 * Constructor Call
+		 * 
+		 * @param array $locations 
+		 */
+		public function __construct($locations) {
+			$this->collection = $locations;
+		}
+
+		/**
+		 * Push new location entry
+		 * 
+		 * @param  decimal $lat  
+		 * @param  decimal $long 
+		 * 
+		 * @return void
+		 */
+		public function push($lat, $long) {
+			array_push([$lat, $long], $this->collection);
+		}
+
+		/**
+		 * Return Location Collection
+		 * 
+		 * @return array
+		 */
+		public function collection() {
+			return $this->collection;
+		}
+
+	}
