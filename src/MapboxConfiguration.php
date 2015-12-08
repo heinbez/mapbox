537b133f6c1f6b8eba853c10f02c8bf925f055c3
Heinrich Bezuidenhout <heinrich@beztdesigns.com>
2015-12-09 05:33:18 +1100

    Create MapboxConfiguration.php

---
+++ src/MapboxConfiguration.php
@@ -0,0 +1,66 @@
+<?php
+	
+	namespace Mapbox;
+
+	class MapboxConfiguration {
+
+		/**
+		 * Mapbox Configuration
+		 * 
+		 * @var array
+		 */
+		public static $config = array();
+
+		/**
+		 * Constructor Call
+		 */
+		public function __construct(array $options) {
+			static::$config = $options;
+		}
+
+		/**
+		 * Return Client Key
+		 * 
+		 * @return String
+		 */
+		public function key() {
+			return static::$config['client']['key'];
+		}
+
+		/**
+		 * Return Client Secret
+		 * 
+		 * @return String
+		 */
+		public function secret() {
+			return static::$config['client']['secret'];
+		}
+
+		/**
+		 * Return Host
+		 * 
+		 * @return string
+		 */
+		public function host() {
+			return static::$config['host'];
+		}
+
+		/**
+		 * Return Style
+		 * 
+		 * @return string
+		 */
+		public function style() {
+			return static::$config['style'];
+		}
+
+		/**
+		 * Return Configuration Array
+		 * 
+		 * @return array
+		 */
+		public function __toString() {
+			return static::$config;
+		}
+
+	}
